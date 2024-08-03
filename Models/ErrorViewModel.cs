4e14e96e15b18d81e39334484bf1d41758996c2c
naxiner <kirienkodenis2002@gmail.com>
2024-08-03 14:32:58 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TripGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
