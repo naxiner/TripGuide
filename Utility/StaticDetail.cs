8d86b95f817e57671c37b4dfc45919ec931a2bce
naxiner <kirienkodenis2002@gmail.com>
2024-08-08 16:36:29 +0300

    Add StaticDetail constants and EmailSender Utilities

---
+++ Utility/StaticDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace TripGuide.Utility
+{
+    public class StaticDetail
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Moderator = "Moderator";
+        public const string Role_User = "User";
+    }
+}
