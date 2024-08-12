83da407e5d9792ac9707a45c851c3517172d66e0
naxiner <kirienkodenis2002@gmail.com>
2024-08-12 18:53:10 +0300

    Add UserRoles view model

---
+++ Models/ViewModels/UserRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TripGuide.Models.ViewModels
+{
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public IList<string> Roles { get; set; } = new List<string>();
+        public IList<string> AllRoles { get; set; } = new List<string>();
+    }
+}
