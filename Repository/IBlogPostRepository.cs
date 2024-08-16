ba659a58754a23cdb84481f01ccba02f1622546f
llllenochkaaa <lenazozula05@gmail.com>
2024-08-16 13:48:48 +0300

    Add hero section, displaying all blogs and their details

--- Repository/IBlogPostRepository.cs
+++ Repository/IBlogPostRepository.cs
@@ -7,6 +7,7 @@ namespace TripGuide.Repositories
         BlogPost Add(BlogPost blogPost);
         IEnumerable<BlogPost> GetAll();
         BlogPost Get(Guid id);
+        BlogPost GetByUrl(string urlHandle);
         BlogPost Update(BlogPost blogPost);
         bool Delete(Guid id);
         IEnumerable<TouristObject> GetAllTouristObjects();
