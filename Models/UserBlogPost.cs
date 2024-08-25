537ff60bc82768e76c5b4669304dc675e5e0ae66
naxiner <kirienkodenis2002@gmail.com>
2024-08-25 18:43:43 +0300

    Add UserBlogPost model and UserProfile view model

---
+++ Models/UserBlogPost.cs
@@ -0,0 +1,11 @@
+﻿namespace TripGuide.Models
+{
+    public class UserBlogPost
+    {
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public Guid BlogPostId { get; set; }
+        public BlogPost BlogPost { get; set; }
+        public string Status { get; set; }
+    }
+}
