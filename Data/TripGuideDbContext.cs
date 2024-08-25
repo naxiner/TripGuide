7c842f651574dce64da32a3cf9ea57ac71c876af
naxiner <kirienkodenis2002@gmail.com>
2024-08-25 18:46:52 +0300

    Add migration for Profile

--- Data/TripGuideDbContext.cs
+++ Data/TripGuideDbContext.cs
@@ -15,8 +15,22 @@ namespace TripGuide.Data
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<Review> Reviews { get; set; }
+        public DbSet<UserBlogPost> UserBlogPosts { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserBlogPost>()
+                .HasKey(ub => new { ub.UserId, ub.BlogPostId });
+
+            modelBuilder.Entity<UserBlogPost>()
+                .HasOne(ub => ub.User)
+                .WithMany(u => u.UserBlogPosts)
+                .HasForeignKey(ub => ub.UserId);
+
+            modelBuilder.Entity<UserBlogPost>()
+                .HasOne(ub => ub.BlogPost)
+                .WithMany(b => b.UserBlogPosts)
+                .HasForeignKey(ub => ub.BlogPostId);
+
             base.OnModelCreating(modelBuilder);
         }
     }
