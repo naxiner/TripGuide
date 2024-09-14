f7951cd4f49598ea80b041d6bdd44c268a3224f8
llllenochkaaa <lenazozula05@gmail.com>
2024-09-14 21:08:32 +0300

    Update HomeViewModel

--- Models/ViewModels/HomeViewModel.cs
+++ Models/ViewModels/HomeViewModel.cs
@@ -4,5 +4,10 @@
     {
         public List<BlogPost> Blogs { get; set; }
         public List<Tag> Tags { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
     }
 }
