ea4a0fac5f479fbe8b5c8fec9d404abb09c5698d
llllenochkaaa <lenazozula05@gmail.com>
2024-09-26 16:29:27 +0300

    Add TouristViewModel for pagination

---
+++ Models/ViewModels/TouristObjectViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TripGuide.Models.ViewModels
+{
+    public class TouristObjectViewModel
+    {
+        public IEnumerable<TouristObject> TouristObjects { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
