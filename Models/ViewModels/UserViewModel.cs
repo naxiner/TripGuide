15e7260706c9d81ab2145b2aaf3106eae1c3bccc
llllenochkaaa <lenazozula05@gmail.com>
2024-09-26 19:36:27 +0300

    Add UserViewModel for pagination

---
+++ Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TripGuide.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public IEnumerable<UserRolesViewModel> Users { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
